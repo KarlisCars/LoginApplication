4791cfb46a1b5c79442944a0ef47b7dcb2902722
Karlis Cars <karlis.cars@gmail.com>
2019-11-10 14:05:45 +0200

    A commit

--- LoginApplication/Controllers/LoginViewController.swift
+++ LoginApplication/Controllers/LoginViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
+    let segueIdentifier = "fireSegue"
 
+    var ref:DatabaseReference!
+    
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var warningLabel: UILabel!
@@ -18,9 +22,49 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    @IBAction func loginButton(_ sender: Any) {
+    
+    func displayWarningLabel(withText text:String){
+        warningLabel.text = text
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: { [weak self] in
+            self?.warningLabel.alpha = 0
+        }) { [weak self] complete in
+            self?.warningLabel.alpha = 1
+        }
     }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        guard let email = emailTextField.text, let password = passwordTextField.text, email != "", password != "" else {
+            displayWarningLabel(withText: "Email/Password incorrect!")
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) {(user, error) in
+        if error != nil {
+            self.displayWarningLabel(withText: "error ocured!")
+            return
+        }
+        if user != nil{
+            self.performSegue(withIdentifier: self.segueIdentifier, sender: nil)
+            return
+        }
+        self.displayWarningLabel(withText: "No such user")
+    }
+   
+    }
+    
     @IBAction func registerButton(_ sender: Any) {
+        guard let email = emailTextField.text, let password = passwordTextField.text, email != "", password != "" else {
+                    displayWarningLabel(withText: "Email/Password incorrect!")
+                    return
+                }
+                Auth.auth().createUser(withEmail: email, password: password, completion:  { (user, error) in
+                    guard error == nil, user != nil else {
+                        print(error!.localizedDescription)
+                        self.displayWarningLabel(withText: "User is not creadted!")
+                        return
+                    }
+        //            let userRef = self.ref.child((user?.user.uid)!)
+        //            userRef.setValue(["email": user?.user.email])
+                })
     }
     
 
